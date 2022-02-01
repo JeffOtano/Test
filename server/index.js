6e56e5e214dace7897a62d117b8946a7f3b5b5ab
Jeffrey Otano <jeotano@microsoft.com>
2022-02-01 16:52:04 -0700

    Fix server issue

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 

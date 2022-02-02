5d68a5e8e366f0d8c34fb7d7b129830e95f0aa20
Jeffrey Otano <jeotano@microsoft.com>
2022-02-01 17:00:20 -0700

    Server changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
@@ -12,4 +14,8 @@ app.get("/api", (req, res) => {
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
